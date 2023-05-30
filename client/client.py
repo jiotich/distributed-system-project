8614ca86cb34a967a3ed9709b17d865f3d69b644
João Francisco Hecksher Olivetti <joao.olivetti@ufv.br>
2023-05-30 17:35:46 -0300

    classes servidor/cliente

--- client/client.py
+++ client/client.py
@@ -1,35 +1,33 @@
 import socket
-import adrs
-import image_ops as imo
 
-HOST = adrs.HOST
-PORT = adrs.PORT
+class Client:
+	def __init__(self):
+		self.HOST = '127.0.0.1'
+		self.PORT = 42068
+		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-PATH = "image.png"
 
-def get_image_packets(path):
-	img_ba = imo.get_bytearray_from_file(path)
-	packets = imo.slice_bytearray(img_ba)
-	return packets
+	def send_to_server(self,message):
+		with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+			# lembrar que sendall() precisa do parametro encoding="utf-8"
+			# caso se esteja trabalhando com texto ao inves de bytes 
+			s.connect((self.HOST,self.PORT))
+			s.sendall(bytes(message))
 
-def send_to_server(message):
-	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-		# lembrar que sendall() precisa do parametro encoding="utf-8"
-		# caso se esteja trabalhando com texto ao inves de bytes 
-		s.connect((HOST,PORT))
-		s.sendall(bytes(message))
+			data = s.recv(1024)
+			print(f"Recieved {data}")
+			s.close()
 
-		data = s.recv(1024)
-		print(f"Recieved {data}")
-		s.close()
-
+x = Client()
+x.send_to_server(b"aaaaa")
+'''
 to_send = get_image_packets(PATH)
 for i in to_send:
 	send_to_server(i)
 
 send_to_server(b"CONN_END")
 
-"""
+
 while True:
 	send_to_server(input("Manda: "))
-"""
+'''
--- server/server.py
+++ server/server.py
@@ -1,32 +1,30 @@
 import socket
-import adrs
-import image_ops as imo
 
-HOST = adrs.HOST
-PORT = adrs.PORT
+class Server:
+	def __init__(self):
+		self.HOST = '127.0.0.1'
+		self.PORT = 42068
+		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.socket.bind((self.HOST, self.PORT))
+		self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-def join_packets_into_image(packets):
-	joined = imo.join_sliced_bytearrays(packets)
-	imo.get_file_from_bytearray(joined)
+	def wait_requests(self):
+		with self.socket as s:
+			s.listen()
+			packets = []
+			data = None
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	
-	s.bind((HOST,PORT))
-	s.listen()
-	
-	packets = []
-	data = None
-	while True:
-		conn, addr, = s.accept()
-		with conn:
-			print(f'Conectado por {addr}')
-			data = conn.recv(1024)
-			if data == b"CONN_END":
-				conn.sendall(bytes("> Transaction ended",encoding='utf-8'))
-				break
-			packets.append(data)
-			conn.sendall(bytes("> Got Packet",encoding='utf-8'))
-			#conn.sendall(bytes("sapa bonde: ",encoding='utf-8')+ data)
-	
-	join_packets_into_image(packets)
+			while True:
+				conn, addr, = s.accept()
+				with conn:
+					print(f'Conectado por {addr}')
+					data = conn.recv(1024)
+					if data == b"CONN_END":
+						conn.sendall(bytes("> Transaction ended",encoding='utf-8'))
+						break
+					packets.append(data)
+					conn.sendall(bytes("> Got Packet",encoding='utf-8'))
+					#conn.sendall(bytes("sapa bonde: ",encoding='utf-8')+ data)
+
+x = Server()			
+x.wait_requests()
