18d8547fdc3d573becb5322a1ab776d1c8393e4d
João Francisco Hecksher Olivetti <joao.olivetti@ufv.br>
2023-06-01 16:46:00 -0300

    criando suporte para operacoes modulares com json

--- client/client.py
+++ client/client.py
@@ -1,25 +1,53 @@
 import socket
+import packet_ops as pops
+import json
 
 class Client:
 	def __init__(self):
 		self.HOST = '127.0.0.1'
-		self.PORT = 42068
-		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.PORT = 42060
+		self.socket = None
 
+	def connect(self):
+		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.socket.connect((self.HOST,self.PORT))
 
 	def send_to_server(self,message):
-		with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-			# lembrar que sendall() precisa do parametro encoding="utf-8"
-			# caso se esteja trabalhando com texto ao inves de bytes 
-			s.connect((self.HOST,self.PORT))
-			s.sendall(bytes(message))
+		self.connect()
+		# lembrar que sendall() precisa do parametro encoding="utf-8"
+		# caso se esteja trabalhando com texto ao inves de bytes 
+		self.socket.sendall(message)
+		data = self.socket.recv(1024)
 
-			data = s.recv(1024)
-			print(f"Recieved {data}")
-			s.close()
+		return data
+	
+	def send_image(self, image_path):
+		response_bytes = self.send_to_server(b'{"operation_request":"send_image"}')
+		response = json.loads(response_bytes)
+		print("Recieved operation token: ", response["response"])
+		if response["response"] == -1:
+			print("> Server refused to recieve image.")
+			return
+		print(1)
+		message = {
+			"operation": response["response"],
+			"image_bytes": pops.get_bytearray_from_file(image_path)
+		}
+		
+		message = bytearray(f"{message}",encoding='utf-8')
+		print(2)
+		self.send_to_server(b"%s" % message)
+		print(2)
+		self.send_to_server(b"CONN_END")
 
-x = Client()
-x.send_to_server(b"aaaaa")
+if __name__ == "__main__":
+	try:
+		x = Client()
+		x.send_image("image.png")
+		#x.send_to_server(b"CONN_END")
+	except KeyboardInterrupt:
+		x.socket.close()
+
 '''
 to_send = get_image_packets(PATH)
 for i in to_send:
--- server/server.py
+++ server/server.py
@@ -1,30 +1,73 @@
 import socket
+import packet_ops as pops
+import json
 
 class Server:
 	def __init__(self):
 		self.HOST = '127.0.0.1'
-		self.PORT = 42068
+		self.PORT = 42060
 		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.socket.bind((self.HOST, self.PORT))
 		self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		self.socket.bind((self.HOST, self.PORT))
 
 	def wait_requests(self):
-		with self.socket as s:
-			s.listen()
-			packets = []
-			data = None
+		self.socket.listen()
+		current_connections = {}
+		packets = []
+		data = None
 
-			while True:
-				conn, addr, = s.accept()
-				with conn:
-					print(f'Conectado por {addr}')
+		while True:
+			conn, addr, = self.socket.accept()
+			with conn:
+				print(f'Conectado por {addr}')
+
+				# o if abaixo representa o inicio de uma operacao requisitada por um cliente
+				# a operacao que sera feita e guardada no dicionario current_connections
+				# para que essa possa em seguida ser feita
+				# ao terminar a operacao, considera-se que o cliente encerrou essa conexao
+				if addr[0] not in current_connections.keys():
+					print(1)
+					current_connections[addr[0]] = None
 					data = conn.recv(1024)
-					if data == b"CONN_END":
-						conn.sendall(bytes("> Transaction ended",encoding='utf-8'))
-						break
-					packets.append(data)
-					conn.sendall(bytes("> Got Packet",encoding='utf-8'))
-					#conn.sendall(bytes("sapa bonde: ",encoding='utf-8')+ data)
+					current_connections[addr[0]] = json.loads(data.decode())
+					conn.sendall(b'{"code":"OK",\n "response":1234}')
+					continue
+				if current_connections[addr[0]]["operation_request"] == "send_image":
+					self.recieve_image()
 
-x = Server()			
-x.wait_requests()
+				data = conn.recv(1024)
+				if data == b"CONN_END":
+					conn.sendall(bytes("> Transaction ended",encoding='utf-8'))
+					break
+				
+
+				#packets.append(data)
+				#conn.sendall(bytes("> Got Packet",encoding='utf-8'))
+
+		#resp = pops.join_sliced_bytearrays(packets).decode()
+		#print(json.loads(resp))
+
+	def recieve_image(self):
+		packets = []
+		data = None
+		while True:
+			print(3)
+			conn, addr, = self.socket.accept()
+			with conn:
+				print(f'Conectado por {addr}')
+				data = conn.recv(1024)
+				if data == b"CONN_END":
+					conn.sendall(bytes("> Transaction ended",encoding='utf-8'))
+					break
+				packets.append(data)
+				conn.sendall(bytes("> Got Packet",encoding='utf-8'))
+		print(packets)
+
+if __name__ == "__main__":
+	x = Server()
+	try:
+		while True:	
+			x.wait_requests()
+	except KeyboardInterrupt:
+		print("> Closing server")
+		x.socket.close()
