0bb1ccc59d6a66a05b8442e74596384ece80cdae
João Francisco Hecksher Olivetti <joao.olivetti@ufv.br>
2023-06-03 15:53:09 -0300

    Login, registro e verificação de login para ops

--- client/client.py
+++ client/client.py
@@ -6,7 +6,7 @@ import base64
 class Client:
 	def __init__(self):
 		self.HOST = '127.0.0.1'
-		self.PORT = 42060
+		self.PORT = 42069
 		self.socket = None
 
 	def connect(self):
@@ -15,13 +15,10 @@ class Client:
 
 	def send_to_server(self,message):
 		self.connect()
-		# lembrar que sendall() precisa do parametro encoding="utf-8"
-		# caso se esteja trabalhando com texto ao inves de bytes 
-		print("> Sending: ", message[0:100])
+		print("> Sending message to server")
 		self.socket.sendall(message)
 		print("> Sent")
 		data = self.socket.recv(1024)
-		print(11,data)
 		return data
 	
 	def send_image(self, image_path):
@@ -35,21 +32,66 @@ class Client:
 			"operation": response["response"],
 			"image_bytes": pops.get_bytearray_from_file(image_path,encode=True).decode()
 		}
-		print(message["image_bytes"])
 		message = bytearray(f"{message}",encoding='utf-8')
 		packets = pops.slice_bytearray(message)
 		print("> Enviando bytearray")
 		for packet in packets:
-			packet = packet.decode('utf-8').replace("\'","\"").encode("utf-8")
+			packet = self.fix_quotes(packet)
 			self.send_to_server(b"%s" % packet)
 		print("> Bytearray enviado")
 		self.send_to_server(b"CONN_END")
 
+	def login(self,username,password):
+		response_bytes = self.send_to_server(b'{"operation_request":"login"}')
+		response = json.loads(response_bytes)
+		print("Recieved operation token: ", response["response"])
+		
+		if response["response"] == -1:
+			print("> Server refused login.")
+			return
+		
+		message = {
+			"operation": response["response"],
+			"username": username,
+			"password": password
+		}
+		message = bytearray(f"{message}",encoding='utf-8')
+		message = self.fix_quotes(message)
+
+		self.send_to_server(message)
+	
+	def register_user(self,username,password):
+		response_bytes = self.send_to_server(b'{"operation_request":"register_user"}')
+		response = json.loads(response_bytes)
+		print("Recieved operation token: ", response["response"])
+		
+		if response["response"] == -1:
+			print("> Server refused registration.")
+			return
+		
+		message = {
+			"operation": response["response"],
+			"username": username,
+			"password": password
+		}
+		message = bytearray(f"{message}",encoding='utf-8')
+		message = self.fix_quotes(message)
+
+		self.send_to_server(message)
+
+	def fix_quotes(self,message):
+		# obviamente tem um jeito melhor de fazer isso
+		# mas estou sem tempo
+		return message.decode('utf-8').replace("\'","\"").encode("utf-8")
+
 if __name__ == "__main__":
+	x = Client()
 	try:
-		x = Client()
-		x.send_image("image.png")
-		x.send_to_server(b"CONN_END")
+		print("> Starting client operations")
+		#x.send_image("image.png")
+		#x.login("marcelinho","mengao123")
+		#x.register_user("thaix","minax")
+
 	except KeyboardInterrupt:
 		x.socket.close()
 
