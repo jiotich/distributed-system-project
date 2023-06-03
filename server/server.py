36c68b5762030757cda94fc3a7d781087aa650b5
João Francisco Hecksher Olivetti <joao.olivetti@ufv.br>
2023-06-03 15:56:44 -0300

    Registro, login e garantia de login para operações

--- server/server.py
+++ server/server.py
@@ -6,36 +6,53 @@ import base64
 class Server:
 	def __init__(self):
 		self.HOST = '127.0.0.1'
-		self.PORT = 42060
+		self.PORT = 42069
 		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 		self.socket.bind((self.HOST, self.PORT))
 		self.socket.listen()
-
+		self.current_connections = {}
+		self.authed_users = {}
 	def wait_requests(self):
-		current_connections = {}
-		packets = []
 		data = None
-
 		while True:
 			conn, addr, = self.socket.accept()
 			with conn:
 				print(f'Conectado por {addr}')
 
-				# o if abaixo representa o inicio de uma operacao requisitada por um cliente
-				# a operacao que sera feita e guardada no dicionario current_connections
-				# para que essa possa em seguida ser feita
-				# ao terminar a operacao, considera-se que o cliente encerrou essa conexao
-				if addr[0] not in current_connections.keys():
-					print(f"> Realizando a primera conexao {addr[0]}")
-					current_connections[addr[0]] = None
-					data = conn.recv(1024)
-					current_connections[addr[0]] = json.loads(data.decode())
+				if addr[0] not in self.current_connections.keys():
+					print(f"> Realizando a primera conexao para {addr[0]}")
+					self.current_connections[addr[0]] = None
+
+				data = conn.recv(1024)
+				operation = json.loads(data.decode())
+				if operation["operation_request"] in ["send_image","load_profile"] and not self.verify_auth(addr[0]):
+					conn.sendall(b'{"code":"DENIED",\n "response":-1}')
+					continue
+				else:
+					self.current_connections[addr[0]] = operation
 					conn.sendall(b'{"code":"OK",\n "response":1234}')
-					#continue
-				if current_connections[addr[0]]["operation_request"] == "send_image":
-					print(f"> Realizando operacao de envio de imagem para {addr[0]}")
+				
+
+				# addr[0] tem o valor do ip, addr[1] o numero da conexao
+				# dicionario dentro de dicionario
+				if self.current_connections[addr[0]]["operation_request"] == "send_image":
+					print(f"> Realizando operacao de recepcao de imagem para {addr[0]}")
 					self.recieve_image()
+					self.operation_finish(addr[0])
+				
+				elif self.current_connections[addr[0]]["operation_request"] == "login":
+					print(f"Logando usuario: {addr[0]}")
+					self.auth_user()
+					self.operation_finish(addr[0])
+				
+				elif self.current_connections[addr[0]]["operation_request"] == "register_user":
+					print(f"Registrando novo usuario: {addr[0]}")
+					self.register_user()
+					self.operation_finish(addr[0])
+
+				elif not self.verify_auth(addr[0]):
+					pass
 
 				data = conn.recv(1024)
 				if data == b"CONN_END":
@@ -66,10 +83,51 @@ class Server:
 				packets.append(data)
 				conn.sendall(bytes("> Got Packet",encoding='utf-8'))
 		
-		x = pops.join_sliced_bytearrays(packets)
-		y = pops.bytearray_to_json(x)
-		print(y["image_bytes"])
-		pops.get_file_from_bytearray(base64.b64decode(y["image_bytes"]))
+		# get_file_from_bytearray() salva o arquivo no diretorio
+		# no futuro, os bytes provavelmente serão armazenados no db
+		loaded_json = pops.bytearray_to_json(pops.join_sliced_bytearrays(packets))
+		pops.get_file_from_bytearray(base64.b64decode(loaded_json["image_bytes"]))
+	
+	def register_user(self):
+		conn, addr, = self.socket.accept()
+		data = b""
+		with conn:
+			print(f'> Registrando usuario para {addr}')
+			data = conn.recv(1024)
+
+		loaded_json = pops.bytearray_to_json(data)
+		
+		# Arquivo de texto usado para propositos de teste
+		# TODO: utilizar o db SQL de fato
+		# TODO: armazenar hashes das senhas ao inves das senhas em si
+		all_users = {}
+		with open("users.json", "r") as db:
+			all_users = json.loads(db.read())
+		
+		if loaded_json["username"] not in all_users:
+			all_users[loaded_json["username"]] = loaded_json["password"]
+		
+		with open("users.json", "w") as file:
+			json.dump(all_users,file)
+
+	def auth_user(self):
+		# TODO: Verificacao de credenciais no banco de dados
+
+		conn, addr, = self.socket.accept()
+		data = b""
+		with conn:
+			print(f'> Validando credenciais para {addr}')
+			data = conn.recv(1024)
+
+		loaded_json = pops.bytearray_to_json(data)
+		self.authed_users[addr[0]] = loaded_json["username"]
+
+	def operation_finish(self,ip):
+		self.current_connections[ip]["operation_request"] = None
+	
+	def verify_auth(self,ip):
+		return 1 if ip in self.authed_users else 0
+	
 
 if __name__ == "__main__":
 	x = Server()
