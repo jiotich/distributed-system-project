835044c6b89e73fee7d4dbf94c995f02c5161c6c
IcaroM-CdC <icaro.moreira@protonmail.com>
2023-06-09 17:24:42 -0300

    aaaaaaaaaaaaaaaaaaaaaa

--- server/core/controller/verify_if_follow_controller.py
+++ server/core/controller/verify_if_follow_controller.py
@@ -4,7 +4,7 @@ from core.services import VerifyIfFollow
 class UpdateUserController:
     def handle(self, follower_username, followed_username):
         verify_if_follow = VerifyIfFollow()
-        response = verify_if_follow.execute(followed_username, followed_username)
+        response = verify_if_follow.execute(follower_username, followed_username)
         
         if (response):
             return json.dumps({"status_code": "200"})
