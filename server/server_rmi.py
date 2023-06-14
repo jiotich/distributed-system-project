a40673df1fb67db5396646122cc257dccef30049
icaroM-CdC <icaro.moreira@protonmail.com>
2023-06-14 11:28:49 -0300

    feat: implemented user operations remote object

--- server/server_rmi.py
+++ server/server_rmi.py
@@ -27,56 +27,67 @@ class UserRemoteObjectInterface:
         self._remove_followed_controller = RemoveFollowedController()
     
     def create_user(self, username, passord, description):
-        self._create_user_controller.handle(
+        response = self._create_user_controller.handle(
             username,
             passord,
             description
         )
+        return response
+        
     def auth_user(self, username, password):
-        self._auth_user_controller.handle(
+        response = self._auth_user_controller.handle(
             username,
             password
         )
+        return response
     
     def find_user(self, username):
-        self._find_user_controller.handle(
+        response = self._find_user_controller.handle(
             username
         )
+        return response
         
     def update_user_description(self, username, new_description):
-        self._update_user_controller.handle(
+        response = self._update_user_controller.handle(
             username,
             new_description,
             column="description"
         )
+        return response
     
     def follow_user(self, followed_username, follower_username):
-        self._follow_user_controller.handle(
+        response = self._follow_user_controller.handle(
             followed_username,
             follower_username
         )
+        return response
 
     def list_user_followers(self, followed_username):
-        self._list_followers_controller.handle(
+        response = self._list_followers_controller.handle(
             followed_username
         )
+        return response
     
     def list_user_followeds(self, follower_controller):
-        self._list_followeds_controller.handle(
+        response = self._list_followeds_controller.handle(
             follower_controller
         )
+        return response
     
     def remove_user_follower(self, followed_username, follower_username):
-        self._remove_follower_controller.handle(
+        response = self._remove_follower_controller.handle(
             followed_username,
             follower_username
         )
+        return response
     
     def remove_user_followed(self, followed_username, follower_username):
-        self._remove_followed_controller.handle(
+        response = self._remove_followed_controller.handle(
             follower_username,
             followed_username
         )
+        return response    
+        
         
 class PostRemoteObjectInterface:
     def __init__(self):
